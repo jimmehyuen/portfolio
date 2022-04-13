d1f2bae52dfe754893aa8857e5e2c774318cbfcc
Jimmy Yuen <jimmehyuen@gmail.com>
2022-04-13 16:46:28 +0100

    finished basic landing page

---
+++ my-portfolio/src/components/intro/intro.jsx
@@ -0,0 +1,32 @@
+import "./intro.css"
+import Me from "../../img/portrait.jpg"
+
+const Intro = () => {
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className= "i-intro">Hello, My name is</h2>
+          <h1 className="i-name">Jimmy</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">UI/UX Designer</div>
+              <div className="i-title-item">Photographer</div>
+              <div className="i-title-item">Reader</div>
+              <div className="i-title-item">Gamer</div>
+            </div>
+          </div>
+          <div className="i-desc">
+            I am a passionate Full-stack Developer based in London
+          </div>
+        </div>
+      </div>
+      <div className="i-right">
+        <img src={Me} alt="" className="i-img" />
+      </div>
+    </div>
+  )
+}
+
+export default Intro
